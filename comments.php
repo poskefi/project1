aa1b5fa0fd6c5c13275d27e2853de893e9ac1e9e
poskefi <poskefi@gmail.com>
2024-10-30 15:20:12 -0400

    Update comments.php

--- comments.php
+++ comments.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Comments - WonderfulResort</title>
+    <title>Comments - DBZ Resort</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">WonderfulResort</a>
+        <a class="navbar-brand" href="index.php">DBZ Resort</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
